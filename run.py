a7fb1b876f5c6d513208a8e0cfe748b55c147b1a
yebente <yebente@gmail.com>
2024-09-02 14:53:08 +0300

    Forms

--- run.py
+++ run.py
@@ -1,7 +1,9 @@
-from app import app, db
-from flask_migrate import Migrate
-
-migrate = Migrate(app, db)
+from app import app
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+from flask_migrate import Migrate
+from app import app, db
+
+migrate = Migrate(app, db)
